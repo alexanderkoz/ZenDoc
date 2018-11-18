60c8849300a8f5d8e3e48fb4b787c4f29402f682
Vitaliy Kril <vkril000@citymail.cuny.edu>
2018-11-18 01:12:19 -0500

    Login and Application Page Done

--- routes/index.js
+++ routes/index.js
@@ -38,12 +38,12 @@ router.post('/register', function(req, res, next) {
   req.checkBody('username', 'Username field cannot be empty.').notEmpty();
   req.checkBody('username', 'Username field cannot be empty.').notEmpty();
   req.checkBody('username', 'Username must be between 4-15 characters long.').len(4, 15);
-  req.checkBody('email', 'The email you entered is invalid, please try again.').isEmail();
-  req.checkBody('email', 'Email address must be between 4-100 characters long, please try again.').len(4, 100);
+  //req.checkBody('email', 'The email you entered is invalid, please try again.').isEmail();
+  //req.checkBody('email', 'Email address must be between 4-100 characters long, please try again.').len(4, 100);
   req.checkBody('password', 'Password must be between 8-100 characters long.').len(8, 100);
   req.checkBody("password", "Password must include one lowercase character, one uppercase character, a number, and a special character.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
-  req.checkBody('passwordMatch', 'Password must be between 8-100 characters long.').len(8, 100);
-  req.checkBody('passwordMatch', 'Passwords do not match, please try again.').equals(req.body.password);
+  //req.checkBody('passwordMatch', 'Password must be between 8-100 characters long.').len(8, 100);
+  //req.checkBody('passwordMatch', 'Passwords do not match, please try again.').equals(req.body.password);
 
   const errors = req.validationErrors();
     if(errors){
@@ -55,15 +55,14 @@ router.post('/register', function(req, res, next) {
         errors: errors
       } );
     } else {
-      const first_name = req.body.first_name;
-      const last_name= req.body.last_name;
+
       const username = req.body.username;
-      const email = req.body.email;
+      const whyOU = req.body.whyOU;
       const password = req.body.password;
 
       const db = require('../db.js');
       //MAKE QUERY TO POST DATA TO database
-      db.query('INSERT INTO user (first_name, last_name, username, email, password) VALUES (?,?,?,?,?)', [first_name, last_name, username, email, password], function(error, results, fields){
+      db.query('INSERT INTO user (username, password, whyOU) VALUES (?,?,?)', [username, password, whyOU], function(error, results, fields){
         if(error) throw error;
 
           db.query('SELECT LAST_INSERT_ID() as user_ID', function(error, results, fields){
