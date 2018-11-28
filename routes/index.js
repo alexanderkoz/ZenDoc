8442f854ca0c3c3a72f9a4a91827b5421469a44d
Vitaliy Kril <vkril000@citymail.cuny.edu>
2018-11-28 12:37:03 -0500

    db bugs fixed with login

--- routes/index.js
+++ routes/index.js
@@ -63,10 +63,10 @@ router.post('/register', function(req, res, next) {
   req.checkBody('username', 'Username field cannot be empty.').notEmpty();
   req.checkBody('username', 'Username field cannot be empty.').notEmpty();
   req.checkBody('username', 'Username must be between 4-15 characters long.').len(1, 15);
-  //req.checkBody('email', 'The email you entered is invalid, please try again.').isEmail();
-  //req.checkBody('email', 'Email address must be between 4-100 characters long, please try again.').len(4, 100);
-  req.checkBody('password', 'Password must be between 8-100 characters long.').len(4, 100);
-  req.checkBody("password", "Password must include one lowercase character, one uppercase character, a number, and a special character.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
+  req.checkBody('email', 'The email you entered is invalid, please try again.').isEmail();
+  req.checkBody('email', 'Email address must be between 4-100 characters long, please try again.').len(4, 100);
+  //req.checkBody('password', 'Password must be between 8-100 characters long.').len(4, 100);
+  //req.checkBody("password", "Password must include one lowercase character, one uppercase character, a number, and a special character.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
   //req.checkBody('passwordMatch', 'Password must be between 8-100 characters long.').len(8, 100);
   //req.checkBody('passwordMatch', 'Passwords do not match, please try again.').equals(req.body.password);
 
@@ -89,7 +89,7 @@ router.post('/register', function(req, res, next) {
 
       const db = require('../db.js');
       //MAKE QUERY TO POST DATA TO database
-      db.query('INSERT INTO users (first_name, last_name, email, username, password) VALUES (?,?,?,?,?)', [first_name, last_name, email, username, password], function(error, results, fields){
+      db.query('INSERT INTO users (first_name, last_name, email, username, password, whyOU) VALUES (?,?,?,?,?,?)', [first_name, last_name, email, username, password, whyOU], function(error, results, fields){
         if(error) throw error;
 
           db.query('SELECT LAST_INSERT_ID() as users_id', function(error, results, fields){
