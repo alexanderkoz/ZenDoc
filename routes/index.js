39d488a7c8dd04a1093c1270277bb2a3bacfedfa
Vitaliy Kril <vkril000@citymail.cuny.edu>
2018-11-28 16:00:13 -0500

    Fixed

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,11 @@ var doc = require('./doc');
 module.exports = function(db) {
   /* GET home page. */
   var files;
- 
+
   router.get('/', function(req, res){
     db.query("SELECT * FROM documents;", (error, results) => {
       if(error) throw error;
-     
+
       files = results.map(function(item) {
         item.url = "/document/" + item.doc_id
         return item;
@@ -21,7 +21,7 @@ module.exports = function(db) {
     });
     console.log(req.user);
     console.log(req.isAuthenticated())
-    
+
   });
 
   router.get('/profile', authenticationMiddleware(),function(req, res){
@@ -72,13 +72,9 @@ module.exports = function(db) {
     req.checkBody('username', 'Username field cannot be empty.').notEmpty();
     req.checkBody('username', 'Username field cannot be empty.').notEmpty();
     req.checkBody('username', 'Username must be between 4-15 characters long.').len(4, 15);
-    //req.checkBody('email', 'The email you entered is invalid, please try again.').isEmail();
-    //req.checkBody('email', 'Email address must be between 4-100 characters long, please try again.').len(4, 100);
-    req.checkBody('password', 'Password must be between 8-100 characters long.').len(8, 100);
-    req.checkBody("password", "Password must include one lowercase character, one uppercase character, a number, and a special character.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
-    //req.checkBody('passwordMatch', 'Password must be between 8-100 characters long.').len(8, 100);
-    //req.checkBody('passwordMatch', 'Passwords do not match, please try again.').equals(req.body.password);
-
+    req.checkBody('email', 'The email you entered is invalid, please try again.').isEmail();
+    req.checkBody('email', 'Email address must be between 4-100 characters long, please try again.').len(4, 100);
+    
     const errors = req.validationErrors();
       if(errors){
 
@@ -123,6 +119,7 @@ module.exports = function(db) {
   passport.deserializeUser(function(user_id, done) {
       done(null, user_id);
   });
+
     function authenticationMiddleware(){
       return (req, res, next)=>{
         console.log( `req.session.passport.user: ${JSON.stringify(req.session.passport)}`);
@@ -133,5 +130,3 @@ module.exports = function(db) {
     }
     return router;
 }
-
-
