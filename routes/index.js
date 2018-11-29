d51a7a716caac46ce69c831e15606a0815dc7258
Vitaliy Kril <vkril000@citymail.cuny.edu>
2018-11-29 00:27:31 -0500

    edited

--- routes/index.js
+++ routes/index.js
@@ -39,7 +39,7 @@ module.exports = function(db) {
 });
 
 
-	
+
   router.get('/login',function(req, res){
     res.render('login', {title: 'Login'});
   });
@@ -68,7 +68,7 @@ module.exports = function(db) {
   router.get('/register', function(req, res, next) {
     res.render('register', { title: 'Registration' });
   });
-  
+
   //Complaint form to complain about a document
 
 router.post('/complaint_doc', function(req, res, next) {
@@ -101,51 +101,56 @@ router.post('/complaint_ou', function(req, res, next) {
 });
 
 
-  router.post('/register', function(req, res, next) {
-    //checn input if its valid
-    req.checkBody('username', 'Username field cannot be empty.').notEmpty();
-    req.checkBody('username', 'Username field cannot be empty.').notEmpty();
-    req.checkBody('username', 'Username must be between 4-15 characters long.').len(4, 15);
-    req.checkBody('email', 'The email you entered is invalid, please try again.').isEmail();
-    req.checkBody('email', 'Email address must be between 4-100 characters long, please try again.').len(4, 100);
-    
-    const errors = req.validationErrors();
-      if(errors){
+router.post('/register', function(req, res, next) {
+//checn input if its valid
+req.checkBody('username', 'Username field cannot be empty.').notEmpty();
+req.checkBody('username', 'Username field cannot be empty.').notEmpty();
+req.checkBody('username', 'Username must be between 4-15 characters long.').len(1, 15);
+req.checkBody('email', 'The email you entered is invalid, please try again.').isEmail();
+req.checkBody('email', 'Email address must be between 4-100 characters long, please try again.').len(4, 100);
+//req.checkBody('password', 'Password must be between 8-100 characters long.').len(4, 100);
+//req.checkBody("password", "Password must include one lowercase character, one uppercase character, a number, and a special character.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
+//req.checkBody('passwordMatch', 'Password must be between 8-100 characters long.').len(8, 100);
+//req.checkBody('passwordMatch', 'Passwords do not match, please try again.').equals(req.body.password);
 
-        console.log(`errors: ${JSON.stringify(errors)}`);
+const errors = req.validationErrors();
+	if(errors){
 
-        res.render('register', {
-          title: 'Registration Error',
-          errors: errors
-        } );
-      } else {
+		console.log(`errors: ${JSON.stringify(errors)}`);
 
-        const username = req.body.username;
-        const whyOU = req.body.whyOU;
-        const password = req.body.password;
+		res.render('register', {
+			title: 'Registration Error',
+			errors: errors
+		} );
+	} else {
+		const first_name = req.body.first_name;
+		const last_name = req.body.last_name
+		const email = req.body.email;
+		const username = req.body.username;
+		const whyOU = req.body.whyOU;
+		const password = req.body.password;
 
-        //const db = require('../db.js');
-        //MAKE QUERY TO POST DATA TO database
-        db.query('INSERT INTO user (username, password, whyOU) VALUES (?,?,?)', [username, password, whyOU], function(error, results, fields){
-          if(error) throw error;
+		const db = require('../db.js');
+		//MAKE QUERY TO POST DATA TO database
+		db.query('INSERT INTO users (first_name, last_name, email, username, password, whyOU) VALUES (?,?,?,?,?,?)', [first_name, last_name, email, username, password, whyOU], function(error, results, fields){
+			if(error) throw error;
 
-            db.query('SELECT LAST_INSERT_ID() as user_ID', function(error, results, fields){
-                if(error) throw error;
+				db.query('SELECT LAST_INSERT_ID() as users_id', function(error, results, fields){
+						if(error) throw error;
 
-                const user_id = results[0];
+						const users_id = results[0];
+						console.log("bbb");
+						//console.log(results[0]);
+						req.login(users_id ,function(err){
+								res.redirect('/login');
 
-                console.log(results[0]);
-                req.login(user_id ,function(err){
-                    res.redirect('/login');
-
-                });
-            })
-        })
-      }
+						});
+				})
+		})
+	}
 
 
-
-  });
+});
 
   passport.serializeUser(function(user_id, done) {
     done(null, user_id);
@@ -166,4 +171,3 @@ router.post('/complaint_ou', function(req, res, next) {
     }
     return router;
 }
-
